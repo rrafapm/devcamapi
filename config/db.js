48df3e775db3a5e8d9b511dda8b5fa940bbd1651
Rafael Peña <rrafapm@gmail.com>
2024-07-05 17:53:30 +0200

    lll

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-// conndb = 'mongodb+srv://ifcd0210rafapm:Eb45ujVpIxW4vtR3@devcam.klinkrl.mongodb.net/?retryWrites=true&w=majority&appName=devcam';
+const conndb = 'mongodb+srv://ifcd0210rafapm:Eb45ujVpIxW4vtR3@devcam.klinkrl.mongodb.net/?retryWrites=true&w=majority&appName=devcam';
 
 const connectDB = async () => {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    // const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(conndb);
     console.log('MongoDB connected');
 }
 
