12be9e71a342b09293534a8c69244a462bb6d3c5
Rafael Peña <rrafapm@gmail.com>
2024-07-05 18:47:25 +0200

    lll

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ const conndb = 'mongodb+srv://ifcd0210rafapm:Eb45ujVpIxW4vtR3@devcam.klinkrl.mon
 
 const connectDB = async () => {
     // const conn = await mongoose.connect(process.env.MONGO_URI);
+    //
     const conn = await mongoose.connect(conndb);
     console.log('MongoDB connected');
 }
